414442ba113a775ef19691a4dfe2b18a1f9120c6
Josh-0115 <sunnyjosh0115@gmail.com>
2025-09-01 20:04:44 +0530

    Uppdated db

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,6 +1,7 @@
 import mysql from "mysql2/promise";
 
 export const db = mysql.createPool({
+  uri: process.env.DATABASE_URL,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,       
